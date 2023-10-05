01dd186cf03e52df6cfc006ee5c6fdfcd165203a
MattPeete1 <mattpeete@gmail.com>
2023-10-05 14:54:55 -0400

    setup auth.
    Co-authored-by: Goke <goke.pelemo@gmail.com>
    Co-authored-by: David <carey2155@gmail.com>
    Co-authored-by: Sweeney <sweenfi@icloud.com>

--- moviement/main_app/urls.py
+++ moviement/main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("movies/<int:pk>", views.MovieDetail.as_view(), name="detail"),
     path("movies/<int:movie_id>/take/create", views.take_create, name="take_create"),
     path("movies/<int:movie_id>/take/add", views.take_add, name="take_add"),
-    
+    path("profile/", views.profile, name="profile"),
+    path("profile/<int:user_id>", views.profile, name="user_profile"),
+    path('profile/signup/', views.signup, name='signup'),
 ]
