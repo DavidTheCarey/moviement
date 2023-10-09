3daa747981e4951f4f329cc71de7ecaf7168e84c
gokepelemo <goke.pelemo@gmail.com>
2023-10-09 18:44:59 -0500

    add settings for local and prod environments

--- moviement/moviement/settings.py
+++ moviement/moviement/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['moviement-gp.onrender.com']
+ALLOWED_HOSTS = ['moviement-gp.onrender.com', 'localhost', 'moviement-q3u3.onrender.com']
 
 
 # Application definition
